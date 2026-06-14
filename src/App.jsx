1f84e48870288476927c5675f844095ddb9dee51
meAtmuna <me.anumta@gmail.com>
2026-06-14 11:49:38 +0500

    basic setup for typing speed tester

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        hahaha
+      </h1>
+    </div>
+  )
+}
+
+export default App
