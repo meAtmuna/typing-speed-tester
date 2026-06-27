0c5f30cc6d3bdfbd467c2e57ff00137dee9fc0ed
meAtmuna <me.anumta@gmail.com>
2026-06-28 02:07:55 +0500

    Update stats UI and try new styles, woohoo... still looks kinda weird

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,18 +1,26 @@
 function Stats({ timeLeft, wpm, mistakes, accuracy }) {
+  const stats = [
+    { label: "TIME" , value: `${timeLeft}s` , color: "text-amber-400"},
+    { label: "WPM" , value: wpm , color: "text-emerald-400"},
+    { label: "ACCURACY" , value: `${accuracy}%` , color: "text-blue-400"},
+    { label: "MISTAKES" , value: mistakes , color: "text-red-400"},
+  ]
     return (
-        <div className="flex justify-center gap-8 mb-8 text-lg">
-          <p className="text-yellow-400">
-            Time: {timeLeft}s
-          </p>
-          <p className="text-green-400">
-            WPM: {wpm}
-          </p>
-          <p className="text-red-500">
-            Mistakes: {mistakes}
-          </p>
-          <p className="text-blue-400">
-            Accuracy:  {accuracy}%
-          </p>
+        <div className="grid md:grid-cols-4 gap-8 mb-8">
+          {stats.map((stat) => (
+            <div
+              key={stat.label}
+              className="bg-card border border-border rounded-xl py-4 mask-l-from-44 mask-r-from-44 text-center"
+            >
+              <p className="text-xs tracking-widest text-muted-text mb-1">
+                {stat.label}
+              </p>
+
+              <p className={`text-3xl font-bold ${stat.color}`}>
+                {stat.value}
+              </p>
+            </div>
+          ))}
         </div>
     )
 }
